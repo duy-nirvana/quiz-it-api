dd0864379f280376cbc9763b9fa3a55870c4cccb
duyTran <duytran@gleads.vn>
2025-03-07 14:05:49 +0700

    add vercel config

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,7 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-// Replace this with your MONGOURI.
-// const MONGOURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nsqjy.mongodb.net/attendance-manage?retryWrites=true&w=majority`;
-const MONGOURI = 'mongodb+srv://duynirvana:KMB7MmAu7Q1ECibx@quiz-it.ag4mg.mongodb.net/';
+const MONGOURI = `mongodb+srv://duynirvana:${process.env.DB_PASSWORD}@quiz-it.ag4mg.mongodb.net/`;
 
 const InitiateMongoServer = async () => {
     try {
